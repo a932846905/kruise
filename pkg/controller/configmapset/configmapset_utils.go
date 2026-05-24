a0da7f12f7790001f143aa2aa817661d97250a51
chrisdeng <chrisdeng@futunn.com>
2026-05-24 17:30:25 +0800

    add cms controller test
    Signed-off-by: chrisdeng <chrisdeng@futunn.com>

--- pkg/controller/configmapset/configmapset_utils.go
+++ pkg/controller/configmapset/configmapset_utils.go
@@ -98,6 +98,11 @@ func GetConfigMapSetPostHookConfigKey(cmsName string) string {
 	return fmt.Sprintf("apps.kruise.io/configmapset-%s-post-hook-config", cmsName)
 }
 
+// GetConfigMapSetContainerStartedAtKey gets the container StartedAt annotation key for CRR
+func GetConfigMapSetContainerStartedAtKey(containerName string) string {
+	return fmt.Sprintf("configmapset.kruise.io/started-at-%s", containerName)
+}
+
 func GetConfigMapSetHubName(cmsName string) string {
 	return fmt.Sprintf("%s-hub", cmsName)
 }
